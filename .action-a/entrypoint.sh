7bb38777c5ddc72a165c5a631599b74fe6503779
Liliana Kastilio <lili@snyk.io>
2019-01-13 13:52:21 +0000

    f

--- .action-a/entrypoint.sh
+++ .action-a/entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/sh -l
 set -e
+echo "Snyk action"
 
 if [ -n "$SNYK_TOKEN" ]; then
+  echo "Installing snyk"
+
   sh -C "npm i -g snyk"
-  sh -c "snyk test $*"
+  echo "Running snyk test"
+  sh -c "snyk test --auth=$SNYK_TOKEN $*"
 fi
 
